956377a22c5111900744588c099ef79f3a0b5e36
LuanPeixotop <luanpeixotopereira@gmail.com>
2018-11-25 17:38:49 -0200

    Feat: Utilizando Post para salvar produtos. Improvement: Layout melhorado seguindo as convenções do rails

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -4,4 +4,9 @@ class ProdutosController < ApplicationController
     @produtos_por_nome = Produto.order(:nome).limit(5)
     @produtos_por_preco = Produto.order(:preco).limit(2)
   end
+
+  def create
+    newProduto = params.require(:produto).permit :nome, :descricao, :quantidade, :preco
+    Produto.create newProduto
+  end
 end
