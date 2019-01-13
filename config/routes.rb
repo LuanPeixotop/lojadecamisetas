b4c0cc1cdbcf598b6f494e16eec60a6d0c1e5be2
LuanPeixotop <luanpeixotopereira@gmail.com>
2019-01-13 17:53:37 -0200

    fix: added index resource route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root "produtos#index"
-  resources :produtos, only: [:create, :destroy, :new]
+  resources :produtos, only: [:create, :destroy, :new, :index]
   get "/produtos/busca" => "produtos#busca", as: :busca_produto
 end
