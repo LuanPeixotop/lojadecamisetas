f8bf78235f7333efc3d06f2057b1828a4789ce21
LuanPeixotop <luanpeixotopereira@gmail.com>
2019-01-20 16:09:04 -0200

    feat: generate deparamento using scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :departamentos
   root "produtos#index"
   resources :produtos, only: [:create, :destroy, :new, :index]
   get "/produtos/busca" => "produtos#busca", as: :busca_produto
